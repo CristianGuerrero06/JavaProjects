25a49628ec9ced38cc9d0bc58ce9c988969e0583
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-08-31 00:37:49 -0500

    Add files via upload

---
+++ PrimerosCodigos/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author WIN10
+ */
+public class HolaMundo {
+    public static void main (string args[]){
+    System.out.print("Hola mundo");
+    
+    }
+    
+}
