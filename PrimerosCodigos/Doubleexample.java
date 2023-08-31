c936689da4623ba5b0aad41c56e59b05f0c90f79
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-08-31 00:34:55 -0500

    Add files via upload

---
+++ PrimerosCodigos/Doubleexample.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package doubleexample;
+
+/**
+ *
+ * @author WIN10
+ */
+public class Doubleexample {
+
+
+    public static void main(String[] args) {
+        double androidShare = -81.7;
+        System.out.println(androidShare);
+    }
+    
+}
