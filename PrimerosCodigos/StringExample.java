76beb3f80c75b0e7189cfb73e75ca7d70d4af6fa
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-08-31 22:29:29 -0500

    Add files via upload

---
+++ PrimerosCodigos/StringExample.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stringexample;
+
+/**
+ *
+ * @author WIN10
+ */
+public class StringExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String openingLyrics = "Yesterday, all my troubles seemed so far away";
+    System.out.println(openingLyrics);
+    }
+    
+}
