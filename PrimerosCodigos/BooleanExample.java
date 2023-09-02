0787980413a127a9d9c75aeeb48d0d77d80272a8
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-09-01 23:45:02 -0500

    Add files via upload

---
+++ PrimerosCodigos/BooleanExample.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package booleanexample;
+
+/**
+ *
+ * @author WIN10
+ */
+public class BooleanExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean intsCanHoldDecimals = false;
+    System.out.println(intsCanHoldDecimals);
+    }
+    
+}
