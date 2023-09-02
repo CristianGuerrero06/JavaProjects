0e929d03f87e98d6ede771a2fe4ae45d9602efb6
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-09-02 00:14:37 -0500

    Add files via upload

---
+++ PrimerosCodigos/ModuleEample.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moduleeample;
+
+/**
+ *
+ * @author WIN10
+ */
+public class ModuleEample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int students = 26;
+    int leftOut = students%3; 
+
+    System.out.println(leftOut);
+    }
+    
+}
