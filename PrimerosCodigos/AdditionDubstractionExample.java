021fc802431f338d30607396b1fc3d7da7d2dd16
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-09-02 00:00:23 -0500

    Add files via upload

---
+++ PrimerosCodigos/AdditionDubstractionExample.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package additiondubstractionexample;
+
+/**
+ *
+ * @author WIN10
+ */
+public class AdditionDubstractionExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    
+    System.out.println(animalsInZoo); 
+
+    int numZebrasAfterTrade = zebrasInZoo - 2;
+
+    System.out.println(numZebrasAfterTrade);
+    }
+    
+}
