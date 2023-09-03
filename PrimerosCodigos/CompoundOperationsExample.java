465f2891b7f0e099c4fecad8d7857002f961ab99
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-09-03 14:19:11 -0500

    Add files via upload

---
+++ PrimerosCodigos/CompoundOperationsExample.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compoundoperationsexample;
+
+/**
+ *
+ * @author WIN10
+ */
+public class CompoundOperationsExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numCookies = 17;
+    numCookies -= 3;
+    System.out.println(numCookies);
+    numCookies /= 2;
+    System.out.println(numCookies);
+    }
+    
+}
