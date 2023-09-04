5a97e06cd7b95d2c4f4e235b82c7a122864f1a60
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-09-04 11:48:22 -0500

    Add files via upload

---
+++ PrimerosCodigos/ConditionalExamples.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conditionalexamples;
+
+/**
+ *
+ * @author WIN10
+ */
+public class ConditionalExamples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int x = 10;    
+        int y = 12;    
+        if(x+y > 20) {    
+            System.out.println("x + y is greater than 20"); 
+    }
+    
+}
