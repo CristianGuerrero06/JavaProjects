b2660b9c3d10152f087d9a4df381dc666f2c9f34
CristianGuerrero06 <142517956+CristianGuerrero06@users.noreply.github.com>
2023-09-03 23:18:38 -0500

    Add files via upload

---
+++ PrimerosCodigos/RelationalOperators2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package relationaloperators2;
+
+/**
+ *
+ * @author WIN10
+ */
+public class RelationalOperators2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int songsA = 9;
+    int songsB = 9;
+    int albumLengthA = 41;
+    int albumLengthB = 53;
+    boolean sameNumberOfSongs = songsA == songsB;
+    boolean differentLength = albumLengthA != albumLengthB;
+    System.out.println(sameNumberOfSongs);
+    System.out.println(differentLength);
+    }
+    
+}
